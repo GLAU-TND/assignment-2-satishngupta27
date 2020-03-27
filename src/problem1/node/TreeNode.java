c64e23728d6a9e02fd237d24d9f2d5f4f518ab38
SATISH <satish.gupta_cs18@gla.ac.in>
2020-03-26 21:22:38 -0700

    created constructor of treenode

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -14,4 +14,30 @@ public class TreeNode {
         this.data = data;
     }
 
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
 }
